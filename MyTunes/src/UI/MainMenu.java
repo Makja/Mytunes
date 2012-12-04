6bf4e54f7828bae2decf4b556111da793c5626c7
iFluX1990 <dalle@iflux.dk>
2012-12-04 12:58:20 +0100

    Added Menu body

--- MyTunes/src/UI/MainMenu.java
+++ MyTunes/src/UI/MainMenu.java
@@ -6,9 +6,48 @@ package UI;
 
 /**
  *
- * @author Daniel
+ * @author bhp
  */
-public class MainMenu
+public class MainMenu extends Menu
 {
-    
+
+    public MainMenu()
+    {
+        super("MyTunes", "Administration",
+                "Control",
+                "....");
+    }
+
+    @Override
+    protected void doAction(int option)
+    {
+        switch (option)
+        {
+            case 1:
+                doActionOption1();
+                break;
+            case 2:
+                doActionOption2();
+                break;
+            case 3:
+                doActionOption3();
+                break;
+        }
+    }
+
+    private void doActionOption1()
+    {
+        new SubMenuAdmin().run();
+    }
+
+    private void doActionOption2()
+    {
+        System.out.println("You selected Control");
+        new SubMenuCon().run();
+    }
+
+    private void doActionOption3()
+    {
+        new SubMenuCon().run();
+    }
 }
