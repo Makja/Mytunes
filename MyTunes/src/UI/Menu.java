4817344bffbc92259795ac8d50ebfcde6e68c69c
iFluX1990 <dalle@iflux.dk>
2012-12-04 13:00:23 +0100

    Added Menu body

--- MyTunes/src/UI/Menu.java
+++ MyTunes/src/UI/Menu.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package menuproject;
+package UI;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
